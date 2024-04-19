b6e1f3511c1987c3871ef2597478e2e7bc3408dd
momo <mhamedbattioui@gmail.com>
2024-04-18 20:02:31 -0400

    c

---
+++ c/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char a[6];
+	char* truc = "enter your strong and complicated password dt";
+	for(int i=0; 1; i++){
+		printf("%c",a[i]);
+	}
+	return 0;
+}
